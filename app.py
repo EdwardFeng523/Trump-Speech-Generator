5c7efb6e7dc35858c3a55150159219f9e58a5671
EdwardFeng523 <jinghaofeng@outlook.com>
2018-07-12 13:03:25 -0500

    edited flask

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import speech_generator
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def get_sentence(word):
+    return "welcome to the trumpifier server"
+
 @app.route('/sentence/<word>', methods=['GET'])
 def get_sentence(word):
     print ("got word", word)
